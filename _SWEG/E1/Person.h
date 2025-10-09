5a016fa17c3dea7c391068a13129590c64d5db3b
Salamanderschatten <stefanpfeiffer14@googlemail.com>
2025-10-09 15:13:53 +0200

    sweg Übung 1

---
+++ _SWEG/E1/Person.h
@@ -0,0 +1,52 @@
+//
+// Created on 09.10.2025.
+//
+
+#pragma once
+
+#include <iostream>
+#include <chrono>
+
+namespace hs_sweg {
+
+    using namespace std;
+
+    class Person {
+    private:
+        string name;
+        string vorname;
+        Date geburtsdatum;
+        int alter;
+
+
+
+    public:
+
+
+    };
+
+
+
+    class Kunde : Person {
+    private:
+        int nr;
+    };
+
+
+
+    class Adresse {
+        string ort;
+        string plz;
+        string strasse;
+        string hausnummer;
+    };
+
+
+
+    class Drucken {
+    private:
+        Adresse* adresse;
+        Kunde* kunde;
+    };
+
+} // hs_sweg
