744ebb4cfa4f7990e93ae51c7a148e2ab8c901ba
Salamanderschatten <stefanpfeiffer14@googlemail.com>
2025-11-10 21:13:43 +0100

    Geng U5

--- _GENG/Unity/Training/Assets/Scripts/avatar/JumpingAvatar.cs
+++ _GENG/Unity/Training/Assets/Scripts/avatar/JumpingAvatar.cs
@@ -1,34 +1,23 @@
-﻿using Unity.VisualScripting;
+﻿using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace avatar
 {
     public class JumpingAvatar : MonoBehaviour
     {
-        public float jumpDuration;
-        public AnimationCurve jumpCourse;
-
         public float speed;
+        
+        
+        private Animator animator;
 
-        private Vector3 lastPos;
-        private Vector3 targetPos;
-        private Quaternion lastRotation;
-        private Quaternion targetRotation;
-        private float progress;
-        private bool jumpActive;
+        private void Awake()
+        {
+            animator = GetComponentInChildren<Animator>();
+        }
 
         public void JumpTo(Vector3 targetPosition)
         {
-            progress = 0;
-            lastPos = transform.position;
-            targetPos = targetPosition;
-            lastRotation = transform.rotation;
-            targetRotation = Quaternion.LookRotation(targetPosition - lastPos, Vector3.up);
-            jumpActive = true;
-
-            GameObject targetObj = new GameObject();
-            targetObj.transform.position = targetPosition;
-            targetObj.transform.rotation = targetRotation;
             
             // Bewegung mit gleichzeitiger sanfter Rotation
             iTween.MoveTo(gameObject, iTween.Hash(
@@ -37,29 +26,24 @@ namespace avatar
                 "easetype", iTween.EaseType.easeInOutSine,
                 "orienttopath", true,
                 "looktime", 0.5f,
-                "axis", "y"
+                "axis", "y",
+                "onstart", "AniStartet",
+                "oncomplete", "AniComplete"
             ));
         }
-
-
-        void Update()
+        
+        
+        private void AniStartet()
         {
-            /*if(jumpActive)
-            {
-                progress += Time.deltaTime / jumpDuration;
-                float relation = jumpCourse.Evaluate(progress);
-                transform.position = Vector3.Lerp(lastPos, targetPos, relation);
-
-
-                transform.rotation = Quaternion.Slerp(
-                    lastRotation,
-                    targetRotation,
-                    relation
-                );
-
-                if (progress >= 1)
-                    jumpActive = false;
-            }*/
+            Debug.Log("AniStartet");
+            animator.SetTrigger("Walk");
+        }
+        
+        
+        private void AniComplete()
+        {
+            Debug.Log("AniComplete");
+            animator.SetTrigger("Stop");
         }
     }
 }
