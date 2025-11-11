e6e72e4ba24b71a3f5e4a6145eeeadd1b3c3d1bc
Salamanderschatten <stefanpfeiffer14@googlemail.com>
2025-11-11 18:43:24 +0100

    CGR U5

--- _CGR/CGR_SORS/CGR_Uebungen/Hightmap/myscene.cpp
+++ _CGR/CGR_SORS/CGR_Uebungen/Hightmap/myscene.cpp
@@ -6,19 +6,22 @@
 #include <windows.h>
 #include <QCoreApplication>
 
-class Heightmap: public SimplePlane
+class Heightmap: public IGeometryImplementation
 {
 public:
     Heightmap();
 protected:
     virtual void fillVertices(QVector<QVector4D>& vertices) override;
     virtual void fillColors(QVector<QVector4D>& colors) override;
+    void render() override;
     QImage image;
+    QVector<QVector4D> colorBuffer; // Speichert Farbwerte für fillColors()
 };
 
 
-Heightmap::Heightmap(): SimplePlane(1.0f) {
-    image.load("motorrad.png");
+Heightmap::Heightmap(): IGeometryImplementation() {
+    QString lPath(SRCDIR); //aus common.cmake !
+    image.load(lPath+"motorrad.png");
     image = image.convertToFormat(QImage::Format_RGB32);
 }
 
@@ -26,26 +29,42 @@ Heightmap::Heightmap(): SimplePlane(1.0f) {
 void Heightmap::fillVertices(QVector<QVector4D>& vertices) {
     vertices.clear();
 
-    for (unsigned int y = 0; y < mDivs; y++)
-    {
-        for (unsigned int x = 0; x <= mDivs; x++)
-        {
-            vertices.append(QVector4D(x / (float)mDivs * mXSize, (y + 1) / (float)mDivs * mYSize, image.pixelColor(x,y).valueF(), 1.0f));
-            vertices.append(QVector4D(x / (float)mDivs * mXSize, y / (float)mDivs * mYSize, 1.0f, 1.0f));
+    int width = image.width();
+    int height = image.height();
+
+    // Normierung, damit die Punkte in einem definierten Bereich liegen
+    float xScale = 1.0f / width * 15;
+    float yScale = 1.0f / height * 15;
+
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            QColor color = image.pixelColor(x, y);
+
+            // Helligkeit berechnen (0.0 - 1.0)
+            float brightness = color.redF() * 0.299f + color.greenF() * 0.587f + color.blueF() * 0.114f;
+
+            // Vertex-Position: z = Helligkeit
+            QVector4D vertex(x * xScale, y * yScale, brightness*15, 1.0f);
+            vertices.append(vertex);
+
+            // Farbe des Pixels speichern
+            colorBuffer.append(QVector4D(color.redF(), color.greenF(), color.blueF(), 1.0f));
         }
     }
 }
 
 void Heightmap::fillColors(QVector<QVector4D>& colors)
 {
-    colors.clear();
-    for (unsigned int y = 0; y < mDivs; y++)
+    colors = colorBuffer;
+}
+
+
+void Heightmap::render() {
+
+
+    for (unsigned int y = 0; y < image.height(); y++)
     {
-        for (unsigned int x = 0; x <= mDivs; x++)
-        {
-            colors.append(QVector4D(x / (float)mDivs * mXSize, (y + 1) / (float)mDivs * mYSize, 1.0f, 1.0f));
-            colors.append(QVector4D(x / (float)mDivs * mXSize, y / (float)mDivs * mYSize, 1.0f, 1.0f));
-        }
+        glFunctions->glDrawArrays(GL_POINTS, y * (2 * image.height() + 2), 2 * image.width() + 2);
     }
 }
 
